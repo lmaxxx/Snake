0f43222e1a8198f7f14bf80e31af725c49917923
Maks-hub256 <61085030+Maks-hub256@users.noreply.github.com>
2021-08-02 10:03:10 +0300

    Third swipe test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
-document.addEventListener('swiped', function(e) {
-  console.log(e.target); // element that was swiped
-  console.log(e.detail.dir); // swipe direction
+import { SwipeEventListener } from 'swipe-event-listener'
+
+const { swipeArea, updateOptions } = SwipeEventListener({
+  swipeArea: document.querySelector('body'),
+});
+
+swipeArea.addEventListener('swipeDown', () => {
+  alert('swipe down');
+});
+swipeArea.addEventListener('swipeUp', () => {
+  alert('swipe up');
+});
+
+swipeArea.addEventListener('swipeLeft', () => {
+  alert('swipe left');
+});
+
+swipeArea.addEventListener('swipeRight', () => {
+  alert('swipe right');
 });
